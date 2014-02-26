5cc8b5ae625d9fe8f6fb52f9ad1dabad1235519a
Timothée Lecomte <lecomte.timothee@gmail.com>
2014-02-26 11:48:40 +0100

    Initial commit

---
+++ plugins/tprovoost/rhino/RhinoPlugin.java
@@ -0,0 +1,9 @@
+package plugins.tprovoost.rhino;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+public class RhinoPlugin extends Plugin implements PluginLibrary
+{
+
+}
